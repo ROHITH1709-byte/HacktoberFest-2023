20dfd26dcbc74776d2106fcf108822a41f7dc0c0
ROHITH1709-byte <24bct140@kgcas.com>
2024-10-19 02:11:05 +0530

    Update binarySearch.py

--- binarySearch.py
+++ binarySearch.py
@@ -1,39 +1,33 @@
-# Python3 code to implement iterative Binary
-# Search.
+# Python3 code to implement iterative Binary Search.
 
+# This function returns the index of x in the array arr if present, otherwise -1
+def binary_search(arr, l, r, x):
+    while l <= r:
+        mid = l + (r - l) // 2  # Find the mid index
 
-# It returns location of x in given array arr
-def binarySearch(arr, l, r, x):
+        # Check if x is present at mid
+        if arr[mid] == x:
+            return mid  # Element found
 
-	while l <= r:
+        # If x is greater, ignore left half
+        elif arr[mid] < x:
+            l = mid + 1
 
-		mid = l + (r - l) // 2
+        # If x is smaller, ignore right half
+        else:
+            r = mid - 1
 
-		# Check if x is present at mid
-		if arr[mid] == x:
-			return mid
+    # If we reach here, then the element was not present
+    return -1
 
-		# If x is greater, ignore left half
-		elif arr[mid] < x:
-			l = mid + 1
-
-		# If x is smaller, ignore right half
-		else:
-			r = mid - 1
-
-	# If we reach here, then the element
-	# was not present
-	return -1
-
-
-# Driver Code
+# Driver code
 if __name__ == '__main__':
-	arr = [2, 3, 4, 10, 40]
-	x = 10
+    arr = [2, 3, 4, 10, 40]
+    x = 10
 
-	# Function call
-	result = binarySearch(arr, 0, len(arr)-1, x)
-	if result != -1:
-		print("Element is present at index", result)
-	else:
-		print("Element is not present in array")
+    # Function call
+    result = binary_search(arr, 0, len(arr) - 1, x)
+    if result != -1:
+        print("Element is present at index", result)
+    else:
+        print("Element is not present in the array")

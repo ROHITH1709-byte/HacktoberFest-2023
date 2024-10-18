95edfc6298b3ca62ec5de9498ff7b1c56f8d5d59
ROHITH1709-byte <24bct140@kgcas.com>
2024-10-18 16:45:48 +0530

    Update Array.py

--- Array.py
+++ Array.py
@@ -1,10 +1,15 @@
 #The following code is for to find Maximum number from an given array
 
-array=list(map(int,input().split()))
+array = list(map(int, input().split()))
 
-arr=array[0]   #By using logic :)
-for i in range(1,len(array)):
-    if arr<array[i]:
-        arr=array[i]
-        
-print(arr)
+if not array:  # Check if the array is empty
+    print("Array is empty.")
+else:
+    arr = array[0]  # Initialize arr with the first element
+    for i in range(1, len(array)):
+        if arr < array[i]:
+            arr = array[i]
+    
+    print(arr)
+
+

c66230327f5613ead7c9c67ba56c4e1ec1056017
ROHITH1709-byte <24bct140@kgcas.com>
2024-10-19 02:15:15 +0530

    Update GuessTheNumber.java

--- java/GuessTheNumber.java
+++ java/GuessTheNumber.java
@@ -1,58 +1,46 @@
 import java.util.Scanner;
 import java.util.Random;
-public class GuessTheNumber
-{
-    public static void main(String [] args)
-    {
-        Random obj=new Random();
-        int a=obj.nextInt(101);
-        System.out.println("Generated Number= "+a);
-        Scanner input=new Scanner(System.in);
+
+public class GuessTheNumber {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int generatedNumber = random.nextInt(101); // Generates a number between 0 and 100
+        System.out.println("Generated Number: " + generatedNumber); // For debugging purposes
+        
+        Scanner input = new Scanner(System.in);
         System.out.println("Number Generated Successfully!!\n\nNow start Guessing the Number.");
-        System.out.println("Are you Ready?? Enter Yes or No.");
-        String b=input.next();
-        int count1=0,count2=0,count3=0;
-        if(b.equalsIgnoreCase("yes"))
-        {
-            for(int i=0;i<50;i++)
-            {
-                System.out.println("Make a Guess.");
-                int x=input.nextInt();
-                if(a-x>0)
-                {
-                    System.out.println("Oops!! not a right guess. \nHint: Increse your Number.");
-                    count1++;
-                    int c1=count1+count2+count3;
-                    System.out.println("No. of attempts= "+c1);
-                    System.out.println("Unsuccessful attempts= "+c1+" Successful attempts= 0");
-                    System.out.println("\nIf you want to continue, go ahead and enter Y for Yes. \nElse,want to leave game enter N for No.");
-                    String c=input.next();
-                    if(c.equalsIgnoreCase("n"))
-                    break;
+        System.out.println("Are you ready? Enter Yes or No.");
+        String response = input.next();
+
+        if (response.equalsIgnoreCase("yes")) {
+            int attempts = 0;
+            boolean gameOn = true;
+
+            while (gameOn) {
+                System.out.println("Make a Guess:");
+                int userGuess = input.nextInt();
+                attempts++;
+
+                if (userGuess < generatedNumber) {
+                    System.out.println("Oops!! Not a right guess. \nHint: Increase your number.");
+                } else if (userGuess > generatedNumber) {
+                    System.out.println("Oops!! Not a right guess. \nHint: Decrease your number.");
+                } else {
+                    System.out.println("Congrats!! You guessed it right.");
+                    break; // Exit loop on correct guess
                 }
-                else if(a-x<0)
-                {
-                    System.out.println("Oops!! not a right guess. \nHint: Decrese your Number.");
-                    count2++;
-                    int c2=count1+count2+count3;
-                    System.out.println("No. of attempts= "+c2);
-                    System.out.println("Unsuccessful attempts= "+c2+" Successful attempts= 0");
-                    System.out.println("\nIf you want to continue, go ahead and enter Y for Yes. \nElse,want to leave game enter N for No.");
-                    String d=input.next();
-                    //count2++;
-                    if(d.equalsIgnoreCase("n"))
-                    break;
-                }
-                else if(a-x==0)
-                {
-                    System.out.println("Congrats!! You Guessed it Right.");
-                    count3++;
-                    int c3=count1+count2+count3;
-                    System.out.println("No. of attempts= "+c3);
-                    System.out.println("Unsuccessful attempts= "+(c3-1)+" Successful attempts= 1.");
-                    break;
+
+                System.out.println("Number of attempts: " + attempts);
+                System.out.println("Do you want to continue? Enter Y for Yes or N for No.");
+                String continueResponse = input.next();
+                if (continueResponse.equalsIgnoreCase("n")) {
+                    gameOn = false; // Exit game if user chooses not to continue
                 }
             }
+        } else {
+            System.out.println("Game cancelled.");
         }
+
+        input.close(); // Close the scanner to avoid resource leaks
     }
-                                       }
+}

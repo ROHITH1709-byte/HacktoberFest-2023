636e812fe8e7c972a432764b8621da11cf8a82ba
ROHITH1709-byte <24bct140@kgcas.com>
2024-10-18 19:28:31 +0530

    Update 3sum.java

--- java/3sum.java
+++ java/3sum.java
@@ -8,8 +8,9 @@ public class ThreeSum {
         Arrays.sort(nums);
 
         for (int i = 0; i < nums.length - 2; i++) {
+            // Skip duplicates for the first element
             if (i > 0 && nums[i] == nums[i - 1]) {
-                continue; // Skip duplicates
+                continue; 
             }
             int target = -nums[i];
             int left = i + 1;
@@ -22,9 +23,32 @@ public class ThreeSum {
                     left++;
                     right--;
 
+                    // Skip duplicates for the second element
                     while (left < right && nums[left] == nums[left - 1]) {
-                        left++; // Skip duplicates
+                        left++; 
                     }
+                    // Skip duplicates for the third element
+                    while (left < right && nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+                } else if (sum < target) {
+                    left++; // We need a larger sum
+                } else {
+                    right--; // We need a smaller sum
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        ThreeSum threeSum = new ThreeSum();
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        List<List<Integer>> result = threeSum.threeSum(nums);
+        System.out.println(result);
+    }
+}
+
                     while (left < right && nums[right] == nums[right + 1]) {
                         right--; // Skip duplicates
                     }

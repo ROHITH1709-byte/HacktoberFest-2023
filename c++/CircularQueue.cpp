5142f09b2faa2b8036fba2692abb761e6c170b29
ROHITH1709-byte <24bct140@kgcas.com>
2024-10-18 21:30:57 +0530

    Update CircularQueue.cpp

--- c++/CircularQueue.cpp
+++ c++/CircularQueue.cpp
@@ -1,117 +1,71 @@
 #include <iostream>
-#define SIZE 5 /* Size of Circular Queue */
+#define SIZE 5 // Size of Circular Queue
 
 using namespace std;
 
 class Queue {
-   private:
-  int items[SIZE], front, rear;
+private:
+    int items[SIZE], front, rear;
 
-   public:
-  Queue() {
-    front = -1;
-    rear = -1;
-  }
-  // Check if the queue is full
-  bool isFull() {
-    if (front == 0 && rear == SIZE - 1) {
-      return true;
-    }
-    if (front == rear + 1) {
-      return true;
-    }
-    return false;
-  }
-  // Check if the queue is empty
-  bool isEmpty() {
-    if (front == -1)
-      return true;
-    else
-      return false;
-  }
-  // Adding an element
-  void enQueue(int element) {
-    if (isFull()) {
-      cout << "Queue is full";
-    } else {
-      if (front == -1) front = 0;
-      rear = (rear + 1) % SIZE;
-      items[rear] = element;
-      cout << endl
-         << "Inserted " << element << endl;
-    }
-  }
-  // Removing an element
-  int deQueue() {
-    int element;
-    if (isEmpty()) {
-      cout << "Queue is empty" << endl;
-      return (-1);
-    } else {
-      element = items[front];
-      if (front == rear) {
+public:
+    Queue() {
         front = -1;
         rear = -1;
-      }
-      // Q has only one element,
-      // so we reset the queue after deleting it.
-      else {
-        front = (front + 1) % SIZE;
-      }
-      return (element);
     }
-  }
 
-  void display() {
-    // Function to display status of Circular Queue
-    int i;
-    if (isEmpty()) {
-      cout << endl
-         << "Empty Queue" << endl;
-    } else {
-      cout << "Front -> " << front;
-      cout << endl
-         << "Items -> ";
-      for (i = front; i != rear; i = (i + 1) % SIZE)
-        cout << items[i];
-      cout << items[i];
-      cout << endl
-         << "Rear -> " << rear;
+    // Check if the queue is full
+    bool isFull() {
+        return (front == 0 && rear == SIZE - 1) || (front == rear + 1);
     }
-  }
-};
 
-int main() {
-  Queue q;
+    // Check if the queue is empty
+    bool isEmpty() {
+        return front == -1;
+    }
 
-  // Fails because front = -1
-  q.deQueue();
+    // Adding an element
+    void enQueue(int element) {
+        if (isFull()) {
+            cout << "Queue is full" << endl;
+        } else {
+            if (front == -1) {
+                front = 0; // Initialize front when first element is added
+            }
+            rear = (rear + 1) % SIZE; // Circular increment
+            items[rear] = element;
+            cout << "Inserted " << element << endl;
+        }
+    }
 
-  q.enQueue(1);
-  q.enQueue(2);
-  q.enQueue(3);
-  q.enQueue(4);
-  q.enQueue(5);
+    // Removing an element
+    int deQueue() {
+        if (isEmpty()) {
+            cout << "Queue is empty" << endl;
+            return -1;
+        } else {
+            int element = items[front];
+            if (front == rear) {
+                front = -1; // Queue is now empty
+                rear = -1;
+            } else {
+                front = (front + 1) % SIZE; // Circular increment
+            }
+            return element;
+        }
+    }
 
-  // Fails to enqueue because front == 0 && rear == SIZE - 1
-  q.enQueue(6);
+    void display() {
+        if (isEmpty()) {
+            cout << "Empty Queue" << endl;
+        } else {
+            cout << "Front -> " << front << endl;
+            cout << "Items -> ";
+            int i = front;
+            while (true) {
+                cout << items[i] << " ";
+                if (i == rear) break; // Exit loop when rear is reached
+                i = (i + 1) % SIZE; // Circular increment
+            }
+            cout << endl << "Rear -> " << rear << endl;
+ 
 
-  q.display();
-
-  int elem = q.deQueue();
-
-  if (elem != -1)
-    cout << endl
-       << "Deleted Element is " << elem;
-
-  q.display();
-
-  q.enQueue(7);
-
-  q.display();
-
-  // Fails to enqueue because front == rear + 1
-  q.enQueue(8);
-
-  return 0;
-}

1df288cedbfc684865867d8e9520b01e7eb8d3cb
ROHITH1709-byte <24bct140@kgcas.com>
2024-10-19 01:56:12 +0530

    Update ball.py

--- python/pong game/ball.py
+++ python/pong game/ball.py
@@ -6,20 +6,24 @@ class Ball(Turtle):
         self.color("white")
         self.shape("circle")
         self.penup()
-        self.x_move=10
-        self.y_move=10
+        self.x_move = 10
+        self.y_move = 10
 
     def move(self):
-        new_x=self.xcor() + self.x_move
-        new_y=self.ycor() + self.y_move
-        self.goto(new_x,new_y)
+        """Move the ball by its current x and y movement values."""
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
 
     def bounce_y(self):
-        self.y_move*=-1
+        """Reverse the y direction of the ball."""
+        self.y_move *= -1
 
     def bounce_x(self):
-        self.x_move*=-1
+        """Reverse the x direction of the ball."""
+        self.x_move *= -1
 
     def reset_position(self):
-        self.goto(0,0)
-        self.bounce_x()
+        """Reset the ball to the center of the screen and reverse its x direction."""
+        self.goto(0, 0)
+        self.bounce_x()  # Ensure the ball moves in the opposite direction after reset

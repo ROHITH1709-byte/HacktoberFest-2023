19dba4d4102ce7b3b7050346c8e74a13e76bf36a
ROHITH1709-byte <24bct140@kgcas.com>
2024-10-18 19:10:20 +0530

    Update Dimond_pattern.cpp

--- c++/Dimond_pattern.cpp
+++ c++/Dimond_pattern.cpp
@@ -1,35 +1,59 @@
 #include <iostream>
 using namespace std;
 
+void printSpaces(int count) {
+    for (int j = 0; j < count; j++) {
+        cout << " ";
+    }
+}
+
+void printStars(int count) {
+    for (int j = 0; j < count; j++) {
+        cout << "*";
+    }
+}
+
+void printDiamond(int n) {
+    // Upper part of the diamond
+    for (int i = 1; i <= n; i += 2) {
+        printSpaces((n - i) / 2);
+        printStars(i);
+        cout << endl;
+    }
+    
+    // Lower part of the diamond
+    for (int i = n - 2; i >= 1; i -= 2) {
+        printSpaces((n - i) / 2);
+        printStars(i);
+        cout << endl;
+    }
+}
+
 int main() {
     int n;
+
     cout << "Enter the number of rows (should be an odd number): ";
-    cin >> n;
+    while (true) {
+        cin >> n;
 
-    if (n % 2 == 0) {
-        cout << "Please enter an odd number for a symmetric diamond pattern."<<endl;
-        return 1;
+        // Input validation
+        if (cin.fail() || n <= 0) {
+            cin.clear(); // Clear the error flag
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Discard invalid input
+            cout << "Invalid input. Please enter a positive odd number: ";
+            continue;
+        }
+
+        if (n % 2 == 0) {
+            cout << "Please enter an odd number for a symmetric diamond pattern." << endl;
+            cout << "Try again: ";
+            continue;
+        }
+
+        break; // Valid input received
     }
 
-    for (int i = 1; i <= n; i += 2) {
-        for (int j = 0; j < (n - i) / 2; j++) {
-            cout << " ";
-        }
-        for (int j = 0; j < i; j++) {
-            cout << "*";
-        }
-        cout <<endl;
-    }
-
-    for (int i = n - 2; i >= 1; i -= 2) {
-        for (int j = 0; j < (n - i) / 2; j++) {
-            cout << " ";
-        }
-        for (int j = 0; j < i; j++) {
-            cout << "*";
-        }
-        cout <<endl;
-    }
+    printDiamond(n);
 
     return 0;
 }

ff172ee6466e8c1905eafc7f822a9660c4c370e5
ROHITH1709-byte <24bct140@kgcas.com>
2024-10-18 17:06:34 +0530

    Update wrapper.java

--- java/wrapper.java
+++ java/wrapper.java
@@ -1,19 +1,25 @@
-//wrapper class program
+public class Wrapper {
+    public static void main(String[] args) {
+        Wrapper x = new Wrapper();
+        Wrapper b = new Wrapper();
+        
+        // Setting references to null to make them eligible for garbage collection
+        x = null;
+        b = null;
+        
+        // Creating another instance
+        new Wrapper();
+        
+        // Suggesting to the JVM to perform garbage collection
+        System.gc(); // Note: This is just a request
+    }
 
-import java.util.*;
-public class wrapper {
-   public static void main(String[]args)
-  {
-    wrapper x = new wrapper();
-    wrapper b = new wrapper();
-    x = null;
-    b = null;
-    new wrapper();
-    System.gc();
-  }
-  public void finalize()
-  {
-    System.out.print("this is GC ");
-
-  }
+    @Override
+    protected void finalize() throws Throwable {
+        try {
+            System.out.println("This is GC");
+        } finally {
+            super.finalize(); // Call to the superclass's finalize method
+        }
+    }
 }

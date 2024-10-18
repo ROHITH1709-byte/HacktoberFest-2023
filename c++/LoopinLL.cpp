84bb24b6ba9bc79a659724499e200f783da9af96
ROHITH1709-byte <24bct140@kgcas.com>
2024-10-18 20:53:02 +0530

    Update LoopinLL.cpp

--- c++/LoopinLL.cpp
+++ c++/LoopinLL.cpp
@@ -1,98 +1,90 @@
 #include <iostream>
 using namespace std;
-class Node
-{
+
+class Node {
 public:
     int data;
     Node *next;
+
+    Node(int val) : data(val), next(nullptr) {} // Constructor to initialize the node
 };
-Node *Insert(int pos,int x,Node *head)
-{
-    Node *t,*p;
-    if(pos==0)
-    {
-        t=new Node;
-        t->data=x;
-        t->next=head;
-        head=t;
-    }
-    else if(pos >0)
-    {
-        p=head;
-        for(int i=0;i<pos-1 &&p;i++)
-        p=p->next;
-        if(p)
-        {
-            t=new Node;
-            t->data=x;
-            t->next=p->next;
-            p->next=t;
+
+Node* Insert(int pos, int x, Node* head) {
+    Node* t = new Node(x); // Create new node
+
+    if (pos == 0) {
+        t->next = head;
+        return t; // New head
+    } else {
+        Node* p = head;
+        for (int i = 0; i < pos - 1 && p; i++) {
+            p = p->next;
+        }
+        if (p) {
+            t->next = p->next;
+            p->next = t;
+        } else {
+            delete t; // Position is out of bounds
+            cout << "Position is out of bounds." << endl;
         }
     }
     return head;
 }
-void Display(Node *head)
-{
-    Node *p=head;
-    while(p)
-    {
-        cout<<p->data<<" ";
-        p=p->next;
+
+void Display(Node* head) {
+    Node* p = head;
+    while (p) {
+        cout << p->data << " ";
+        p = p->next;
     }
+    cout << endl; // New line after displaying the list
 }
-bool Loop(Node *head)
-{
-    bool var;
-    Node *p,*q;
-    p=head;
-    q=head;
-    // while(p && q && p!=q)
-    // {
-    //     p=p->next;
-    //     q=q->next;
-    //     if(q!=NULL)
-    //     q->next;
-    //     else
-    //     q=q;
-    // }    
-    // if(p==q)
-    // var =true;
-    // else
-    // var = false;
-    do
-    {
-    p=p->next;
-    q=q->next;
-    q=q?q->next:q;
+
+bool Loop(Node* head) {
+    if (!head) return false; // No nodes in the list
+
+    Node *slow = head, *fast = head;
+
+    while (fast && fast->next) {
+        slow = slow->next; // Move slow pointer by 1
+        fast = fast->next->next; // Move fast pointer by 2
+
+        if (slow == fast) { // Loop detected
+            return true;
+        }
     }
-    while(p && q && p!=q);
-    if(p==q)
-    var=true;
+    return false; // No loop
+}
+
+int main() {
+    Node* first = nullptr;
+    int x, n;
+
+    cout << "Enter the number of elements you want to enter: ";
+    cin >> n;
+    
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        first = Insert(i, x, first);
+    }
+
+    // Display the linked list
+    cout << "Linked List: ";
+    Display(first);
+
+    // Check for loop
+    if (Loop(first))
+        cout << "Linked List has a loop." << endl;
     else
-    var=false;
-    return var;
-}
-int main()
-{
-Node *first=NULL;
-int x;
-int n;
-cout<<" Enter the Number of elements you want to enter "<<endl;
-cin>>n;
-for(int i=0;i<n;i++)
-{
-    cin>>x;
-    first=Insert(i,x,first);
-}
-// Display(first);
-// cout<<endl;
-Node *t1,*t2;
-// t1=first->next->next;
-// t2=first->next->next->next->next;
-// t2->next=t1;
-if(Loop(first))
-cout<<" linked List has a Loop "<<endl;
-else
-cout<<" linked list does not have a Loop "<<endl;
-return 0;
+        cout << "Linked List does not have a loop." << endl;
+
+    // Memory cleanup
+    Node* current = first;
+    while (current) {
+        Node* nextNode = current->next;
+        delete current; // Free allocated memory
+        current = nextNode;
+    }
+
+    return 0;
 }
